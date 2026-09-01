33ea9c37e6eaa75ff88b91ac3c86176d8b3c3f22
Bernardo Sk <bimski1056@gmail.com>
2026-08-31 22:15:38 -0300

    testando fluxo linear pressao pressao (confirmar depois)

---
+++ Trabalho 1/fluxo_linear_pressao.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Dados
+k = 1*(10**(-13)) #permeabilidade
+phi = 0.2 #porosidade
+mu = 1*(10**(-3)) #viscosidade 
+ct = 1*(10**(-9)) #compressibilidade total
+L_reservatorio = 100 #comprimento
+pw = 15*(10**6) 
+pe = 25*(10**6)
+
+dh = k / (phi * mu * ct) #Difusividade Hidraulica
+
+#Fazendo fluxo linear
+def linear_pressao_pressao(x, t, L, pw, pe, termos=100):
+    soma = np.zeros_like(x)
+    for n in range (1, termos + 1):
+        exp = np.exp(-((n * np.pi / L)**2) *dh *t)
+        seno = np.sin(n * np.pi * x / L)
+        soma += (1/n) * exp * seno
+
+    p = (pe - pw) * ((x/L) + (2 / np.pi) * soma) + pw
+    return p
+
+
+x = np.linspace(0, L_reservatorio, 100)
+t_dias = [10, 50, 100]
+for t in t_dias:
+    #t_segs = t * 86400 #passar para segundos por conta da difusividade hidráulica
+    f = linear_pressao_pressao(x, t, L_reservatorio, pw, pe)
+    p = f/(10**6) #conversão para MPa
+    plt.plot(x, p, label=f't = {t}')
+
+plt.title('Fluxo Linear Pressão Pressão')
+plt.xlabel('Distância (m)')
+plt.ylabel('Pressão (MPa)')
+plt.legend()
+plt.grid(True)
+plt.show()
